08adf55b3e88138c3ab34f229fe24096a3e9a3ef
Robert McClintick <robert.mcc3@gmail.com>
2022-05-24 13:12:39 -0400

    complete sumAll using loops and typeOf.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    result = 0;
+    if (num1 < 0 || num2 < 0 || typeof(num1) != 'number' || typeof(num2) != 'number')
+        return 'ERROR'
+    else if (num1 > num2)
+    {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for (let i = num1; i <= num2; i++)
+    {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
