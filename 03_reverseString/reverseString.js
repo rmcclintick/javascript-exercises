85f117ffeb4abfddfa4b09b53854990fff63eb50
Robert McClintick <robert.mcc3@gmail.com>
2022-05-23 21:08:10 -0400

    complete reverseString. passes all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let chars = Array.from(string)
+    let result = "";
 
+    for (const current of chars)
+    {
+        result = current + result;
+    }  
+    return result;
 };
 
 // Do not edit below this line
