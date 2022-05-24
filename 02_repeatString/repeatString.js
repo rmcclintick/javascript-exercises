53e2cdd0a61c7d9e082b3c3eeeda46637c918845
Robert McClintick <robert.mcc3@gmail.com>
2022-05-23 20:36:34 -0400

    Complete repeatString. passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0)
+        return 'ERROR';
+    else 
+    {
+        let result = '';
+        for(let i = 0; i < count; i++)
+        {
+            result = `${result}${string}`;
+        }
+    
+        return result;
+    }
+    
 };
 
 // Do not edit below this line
