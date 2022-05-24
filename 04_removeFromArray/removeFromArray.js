de9ef96cf2d4dc8bf2a78215b9b80b7e8c160b27
Robert McClintick <robert.mcc3@gmail.com>
2022-05-23 21:36:29 -0400

    can remove a single element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, numToRemove) {
+    console.log(array);
+    return array.filter(function(element){
+        return element != numToRemove;
+    });
+    console.log(array);
 };
 
 // Do not edit below this line
