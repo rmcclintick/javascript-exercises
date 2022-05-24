47a199b3d7f5c2fc769a7163cf90d7aaccd3d13b
Robert McClintick <robert.mcc3@gmail.com>
2022-05-24 13:03:15 -0400

    complete removeFromArray. passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,18 @@
-const removeFromArray = function(array, numToRemove) {
-    console.log(array);
-    return array.filter(function(element){
-        return element != numToRemove;
-    });
-    console.log(array);
+const removeFromArray = function(array, ...numsToRemove) {
+    console.log(array, numsToRemove);
+    return array.filter(value => !numsToRemove.includes(value));
+    //array = array.filter(function(element){
+        
+
+        // for (let i = 0; i < numsToRemove.length; i++)
+        // {
+        //     if (element == numsToRemove[i])
+        //         return false;
+        //     else return true;
+        // }
+    //});
+    //console.log(array);
+    //return array;
 };
 
 // Do not edit below this line
